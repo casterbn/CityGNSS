a50d45e3f75e764f2b3a3211399e9303775a4661
Tiago Maia <gtiago.maia@gmail.com>
2019-04-11 14:52:30 +0100

    Initial Commit

---
+++ SNLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNLocation
+//
+//  Created by Tiago Maia on 11/04/2019.
+//  Copyright © 2019 Tiago Maia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
